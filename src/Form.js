c1e608e51ceda8a6863c64631f2751b1db58c4fa
Vlada Kandyba <hello.vladasana@gmail.com>
2022-04-28 11:00:08 +0200

    added API and axios

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import axios from "axios";
 
 export const Form = () => {
   const [word, setWord] = useState("");
+  const handleResponse = (response) => {
+    console.log(response.data[0]);
+  };
+
   const search = (e) => {
     e.preventDefault();
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   };
   const handleInputChange = (e) => {
     e.preventDefault();
